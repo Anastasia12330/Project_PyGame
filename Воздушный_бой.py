7b8a0bb8a099bd429efe295c15ac35c6db4e12d8
Smart developer <developer@yandex.ru>
2019-02-20 15:20:54 +0300

    Корректируем и дополняем программу

--- Воздушный_бой.py
+++ Воздушный_бой.py
@@ -5,12 +5,13 @@ size = width, height = 1000, 600
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 
+pygame.time.set_timer(pygame.USEREVENT, 3000)
+
 
 def load_image(name, color_key=-1):
     fullname = os.path.join('data', name)
     try:
         image = pygame.image.load(fullname)
-        # image = image.convert_alpha()
     except pygame.error as message:
         print('Cannot load image:', name)
         raise SystemExit(message)
@@ -47,12 +48,46 @@ class AnimatedSprite(pygame.sprite.Sprite):
         self.image = self.frames[self.cur_frame]
 
 
+# создаем класс космических кораблей ПРОТИВНИКА
+class SpaceShip(pygame.sprite.Sprite):
+    image = load_image("fw190a8.jpg", -1)
+    image = pygame.transform.scale(image, (100, 50))
+
+    def __init__(self, group):
+        super().__init__(group)
+        self.image = SpaceShip.image
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.x = width
+        self.rect.y = random.randint(30, height - y1)
+        self.speed = 3
+        self.kill()
+        while pygame.sprite.spritecollideany(self, all_SpaceShip):
+            self.rect.x = width
+            self.rect.y = random.randrange(30, height - y1)
+        all_SpaceShip.add(self)
+
+    def update(self):
+        if self.rect.x > 0:
+            self.rect.x -= self.speed
+        # убиваем, если корабль ушел за экран
+        elif self.rect.x < 0:
+            self.kill()
+
+
 # создадим группу, содержащую все спрайты
 all_sprites = pygame.sprite.Group()
 all_dragon = pygame.sprite.Group()
 
-helicopter = AnimatedSprite(load_image("helicopter.png"), 1, 4, 50, 150)
+helicopter = AnimatedSprite(load_image("helicopter.png", -1), 1, 4, 50, 150)
 all_sprites = pygame.sprite.Group(helicopter)
+
+# создадим группу, содержащую все корабли ПРОТИВНИКА
+all_SpaceShip = pygame.sprite.Group()
+
+SpaceShip_image = load_image("fw190a8.jpg", -1)
+SpaceShip_image = pygame.transform.scale(SpaceShip_image, (100, 50))
+x1, y1 = SpaceShip_image.get_rect().size
 
 fon = pygame.transform.scale(load_image("Mountain.jpg"), (width, height))
 
@@ -64,10 +99,15 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        if event.type == pygame.USEREVENT:  # появление кораблей ПРОТИВНИКА
+            for i in range(1):
+                SpaceShip(all_SpaceShip)
+
     all_sprites.update()
     all_sprites.draw(screen)
 
-    all_dragon.update()
-    all_dragon.draw(screen)
+    all_SpaceShip.draw(screen)
+    all_SpaceShip.update()
+
     pygame.display.flip()
     clock.tick(20)
